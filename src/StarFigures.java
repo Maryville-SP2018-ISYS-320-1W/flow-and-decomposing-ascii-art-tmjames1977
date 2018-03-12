590623c333144e24b63c55da2607b611939d7d8e
Joe Dent <jdent@maryville.edu>
2018-03-12 01:11:33 -0500

    Initial commit

---
+++ src/StarFigures.java
@@ -0,0 +1,16 @@
+/*
+  	ISYS 320
+  	Name(s):
+  	Date: 
+*/
+
+// 4. Your pseudocode algorithm for how to break down the figure
+
+
+public class StarFigures {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/Tricky.java
@@ -0,0 +1,17 @@
+/*
+  	ISYS 320
+  	Name(s):
+  	Date: 
+*/
+
+// 1. Your predicted output 
+ 
+
+public class Tricky {
+
+}
+
+// 3. Were you correct? Explain any differences
+ 
+ 
+
